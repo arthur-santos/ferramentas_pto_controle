cbf11bc94316b404c71eb70b1f8c1d47adb1f196
elitonfilho <elitonfilho@gmail.com>
2020-01-08 11:44:59 -0300

    fix: Update sequence of tools on QGIS

--- loadToBPC/loadToBPC.py
+++ loadToBPC/loadToBPC.py
@@ -153,7 +153,7 @@ class LoadToBPC(QgsProcessingAlgorithm):
         lowercase alphanumeric characters only and no spaces or other
         formatting characters.
         """
-        return '9- Preparar insumos para carregamento no BPC'
+        return '10- Preparar insumos para carregamento no BPC'
 
     def displayName(self):
         """
